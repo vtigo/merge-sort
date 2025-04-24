75450499b4ed96883032333e39fa89e1a3e36ec7
tigo <rf.tigas@gmail.com>
2025-04-24 16:36:19 -0300

    implement merge sort recursive algorithm

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vtigo/merge-sort
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := []int{9, 3, 5, 7, 2, 8, 1, 4, 6}
+	
+	sortedC := mergeSort(c)
+
+	fmt.Printf("unsorted: %v", c)
+	fmt.Printf("sorted: %v", sortedC)
+}
+
+func mergeSort(c []int) []int {
+	if len(c) < 2 {
+		return c
+	}
+	
+	a := mergeSort(c[:len(c)/2])
+	b := mergeSort(c[len(c)/2:])
+	return merge (a, b)
+}
+
+func merge(a []int, b []int) []int {
+	c := make([]int, len(a) + len(b))
+	i := 0
+	j := 0
+
+	for k := range c {
+		if i >= len(a) {
+			c[k] = b[j]
+			j++
+		} else if j >= len(b) {
+			c[k] = a[i]
+			i++
+		} else if a[i] < b[j] {
+			c[k] = a[i]
+			i++
+		} else {
+			c[k] = b[j]
+			j++
+		}
+	}
+
+	return c
+}
+
+
